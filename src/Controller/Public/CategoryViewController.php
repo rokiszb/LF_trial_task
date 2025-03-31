8a4cdbd3f2c4035f42347ab04447d7c2f439c31f
RokasL <rokas.lakstauskas@gmail.com>
2025-04-01 02:28:21 +0300

    css styling

--- src/Controller/Public/CategoryViewController.php
+++ src/Controller/Public/CategoryViewController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Public;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use App\Repository\NewsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,51 +15,25 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/category')]
 final class CategoryViewController extends AbstractController
 {
-
     #[Route(name: 'app_category_index', methods: ['GET'])]
-public function index(CategoryRepository $categoryRepository): Response
-{
-    return $this->render('public/category/show.html.twig', [
-        'categories' => $categoryRepository->findAll(),
-    ]);
-}
-
-    #[Route('/new', name: 'app_category_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($category);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('category/new.html.twig', [
-            'category' => $category,
-            'form' => $form,
+        return $this->render('public/category/show.html.twig', [
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 
     #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
-    public function show(Category $category): Response
+    public function show(Category $category, Request $request, NewsRepository $newsRepository): Response
     {
+        $page = $request->query->getInt('page', 1);
+        $limit = 10;
+
+        $news = $newsRepository->findByCategoryPaginated($category, $page, $limit);
+
         return $this->render('public/category/show.html.twig', [
             'category' => $category,
+            'news' => $news,
         ]);
-    }
-
-    #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
-    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($category);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
     }
 }
